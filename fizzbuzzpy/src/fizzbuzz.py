5cfb35ae2ac3b732608f95de56faf6a5af309f90
Rhys Kyte <rskyte@gmail.com>
2019-06-02 22:18:46 +0100

    returns 0 correctly + test failure global added

--- fizzbuzzpy/src/fizzbuzz.py
+++ fizzbuzzpy/src/fizzbuzz.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 def fizzbuzz(num):
-    if num % 3 == 0 and num % 5 == 0:
+    if num == 0:
+        return '0'
+    elif num % 3 == 0 and num % 5 == 0:
         return 'fizzbuzz'
     elif num % 5 == 0:
         return 'buzz'
     elif num % 3 == 0:
         return 'fizz'
+    
--- fizzbuzzpy/test/test_fizzbuzz.py
+++ fizzbuzzpy/test/test_fizzbuzz.py
@@ -4,26 +4,30 @@ import unittest
 sys.path.append('./fizzbuzzpy')
 from src.fizzbuzz import fizzbuzz
 
+FAILURE = 'incorrect value'
+
 class FizzbuzzTest(unittest.TestCase):
     def test_returns_fizz_for_multiple_of_3(self):
         value = fizzbuzz(3)
         value2 = fizzbuzz(9)
-        self.assertEqual(value, 'fizz', 'incorrect value')
-        self.assertEqual(value2, 'fizz', 'incorrect value')
+        self.assertEqual(value, 'fizz', FAILURE)
+        self.assertEqual(value2, 'fizz', FAILURE)
 
     def test_returns_fizz_for_multiple_of_5(self):
         value = fizzbuzz(5)
         value2 = fizzbuzz(25)
-        self.assertEqual(value, 'buzz', 'incorrect value')
-        self.assertEqual(value2, 'buzz', 'incorrect value')
+        self.assertEqual(value, 'buzz', FAILURE)
+        self.assertEqual(value2, 'buzz', FAILURE)
 
     def test_returns_fizzbuzz_for_multiple_of_3_and_5(self):
         value = fizzbuzz(15)
         value2 = fizzbuzz(75)
-        self.assertEqual(value, 'fizzbuzz', 'incorrect value')
-        self.assertEqual(value2, 'fizzbuzz', 'incorrect value')
+        self.assertEqual(value, 'fizzbuzz', FAILURE)
+        self.assertEqual(value2, 'fizzbuzz', FAILURE)
 
-
+    def test_returns_zero_for_zero(self):
+        value = fizzbuzz(0)
+        self.assertEqual(value, '0', FAILURE)
 
 if __name__ == '__main__':
     unittest.main()
